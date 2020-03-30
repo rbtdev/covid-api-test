043d860172c003bd6a51d48b63a4475f11bdfac6
Rob Thuleen <robert@diversyfund.com>
2020-03-30 15:51:30 -0700

    Updates

---
+++ precache-manifest.0d8c31c912c90158e00e1a33549d3676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31721f2f93581bc8bab4a936b17d282",
+    "url": "/covid-api-test/index.html"
+  },
+  {
+    "revision": "547849b82dd739cfe96d",
+    "url": "/covid-api-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac6e096fe819de7013de",
+    "url": "/covid-api-test/static/js/2.2ad83365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-api-test/static/js/2.2ad83365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547849b82dd739cfe96d",
+    "url": "/covid-api-test/static/js/main.3ea1c764.chunk.js"
+  },
+  {
+    "revision": "3780daee11e5ab4cf810",
+    "url": "/covid-api-test/static/js/runtime-main.e01e48c7.js"
+  }
+]);
